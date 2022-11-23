2b929b3704e914869fb36b720040f189c86e6553
Hunter Borlik <hborlik@users.noreply.github.com>
2022-11-23 10:56:24 -0800

    feature tracks

--- src/feature.py
+++ src/feature.py
@@ -2,5 +2,7 @@ import cv2 as cv
 
 class Feature:
 
-    def __init__(self, frame : cv.UMat) -> None:
+    def __init__(self, frame : cv.UMat, kp, desc) -> None:
         self.frame = frame
+        self.keypoints = kp
+        self.descriptors = desc
