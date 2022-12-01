ece6d3f8e0be47431adc863f92f581f9e8ee6eb4
Hunter Borlik <hborlik@users.noreply.github.com>
2022-12-01 12:24:53 -0800

    FeatureExtractor

--- src/build_feature_set.py
+++ src/build_feature_set.py
@@ -27,6 +27,85 @@ def rotation_from_H(H):
 def theta_from_R(R):
     return math.atan2(R[1,0], R[0,0])
 
+class FeatureExtractor:
+    def __init__(self, feature_frames : int) -> None:
+        self.orb = cv.ORB_create()
+
+        self.feature_collection = []
+        self.feature_votes = []
+
+        self.feature_frames = feature_frames
+
+
+    def add_feature_frame(self, frame : cv.UMat) -> None:
+        new_fr_kp = self.orb.detect(frame, None)
+        new_fr_kp, new_fr_desc = self.orb.compute(frame, new_fr_kp)
+
+        self.feature_collection.append(feature.Feature(new_fr_kp, new_fr_desc, frame))
+
+    def reduce_features(self):
+        for i, feature_fr in enumerate(self.feature_collection):
+            votes = np.zeros((feature_fr.descriptors.shape[0]))
+            a = max(0, i-self.feature_frames)
+            b = min(len(self.feature_collection), i+self.feature_frames)
+            for j in range(a, b):
+                if j == i:
+                    continue
+                fr = self.feature_collection[j]
+                # feature1 : feature.Feature = feature_collection[-1]
+                matches = flann_matcher.knnMatch(np.float32(feature_fr.descriptors), np.float32(fr.descriptors), k=2)
+
+                # store all the good matches as per Lowe's ratio test.
+                good = []
+                for m,n in matches:
+                    if m.distance < 0.7*n.distance:
+                        good.append(m)
+
+                if len(good) < 4:
+                    continue
+
+                for match in good:
+                    votes[match.queryIdx] += 1
+
+                # n_fr_pts, fr_pts = map(list, zip(*[(np.array(new_feature_fr.keypoints[match.queryIdx].pt)[np.newaxis, :], np.array(fr.keypoints[match.trainIdx].pt)[np.newaxis, :]) for match in good]))
+                # n_fr_pts = np.stack(n_fr_pts, axis=0)
+                # fr_pts = np.stack(fr_pts, axis=0)
+
+                # H, mask = cv.findHomography(n_fr_pts, fr_pts, method=cv.RANSAC, ransacReprojThreshold=5.0)
+                # mask = mask.ravel().tolist()
+
+                draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                                singlePointColor = None,
+                                # matchesMask = mask, # draw only inliers
+                                flags = 2)
+
+                im3 = cv.drawMatches(feature_fr.frame, feature_fr.keypoints, fr.frame, fr.keypoints, good, None, **draw_params)
+
+                cv.imshow("tracked", im3)
+                if cv.waitKey(1) == 27: # escape
+                    stop = True
+                    break
+            
+            self.feature_votes.append(votes)
+
+        assert(len(self.feature_votes) == len(self.feature_collection))
+
+        total_features = 0
+        final_features = 0
+        for f in self.feature_collection:
+            total_features += len(f.descriptors)
+
+        print("Reducing feature count")
+        for f, vote in zip(self.feature_collection, self.feature_votes):
+            f.descriptors = f.descriptors[vote > 0]
+            f.keypoints = [x for x, y, in zip(f.keypoints, vote) if y > 0]
+
+        for f in self.feature_collection:
+            final_features += len(f.descriptors)
+
+        print("%i features down from %i" % (final_features, total_features))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', required=True)
@@ -37,10 +116,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     cap = cv.VideoCapture(args.input)
-
-    feature_collection = []
-    feature_matches = []
-    feature_votes = []
+    
+    fextractor = FeatureExtractor(args.feature_frames)
 
     cv.namedWindow('tracked', flags=cv.WINDOW_NORMAL | cv.WINDOW_KEEPRATIO)
     cv.resizeWindow('tracked', 500, 500)
@@ -58,95 +135,10 @@ if __name__ == "__main__":
         if not ret:
             break
         if (frame_id % args.frame_skip) == 0:
-
-            orb = cv.ORB_create()
-            new_fr_kp = orb.detect(frame, None)
-
-            new_fr_kp, new_fr_desc = orb.compute(frame, new_fr_kp)
-
-            new_feature_fr = feature.Feature(new_fr_kp, new_fr_desc, frame)
-
-            # img2 = cv.drawKeypoints(frame, new_fr_kp, None, color=(0, 255, 0), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            # cv.imshow('tracked', img2)
-            
-            if len(feature_collection) > args.feature_frames:
-                votes = np.zeros((new_feature_fr.descriptors.shape[0]))
-                for i, fr in enumerate(feature_collection[-args.feature_frames:]):
-                    # feature1 : feature.Feature = feature_collection[-1]
-                    matches = flann_matcher.knnMatch(np.float32(new_feature_fr.descriptors), np.float32(fr.descriptors), k=2)
-
-                    # store all the good matches as per Lowe's ratio test.
-                    good = []
-                    for m,n in matches:
-                        if m.distance < 0.7*n.distance:
-                            good.append(m)
-
-                    if len(good) < 4:
-                        continue
-
-                    for match in good:
-                        votes[match.queryIdx] += 1
-
-                    # n_fr_pts, fr_pts = map(list, zip(*[(np.array(new_feature_fr.keypoints[match.queryIdx].pt)[np.newaxis, :], np.array(fr.keypoints[match.trainIdx].pt)[np.newaxis, :]) for match in good]))
-                    # n_fr_pts = np.stack(n_fr_pts, axis=0)
-                    # fr_pts = np.stack(fr_pts, axis=0)
-
-                    # H, mask = cv.findHomography(n_fr_pts, fr_pts, method=cv.RANSAC, ransacReprojThreshold=5.0)
-                    # mask = mask.ravel().tolist()
-
-                    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                                    singlePointColor = None,
-                                    # matchesMask = mask, # draw only inliers
-                                    flags = 2)
-
-                    im3 = cv.drawMatches(new_feature_fr.frame, new_feature_fr.keypoints, fr.frame, fr.keypoints, good, None, **draw_params)
-
-                    cv.imshow("tracked", im3)
-                    if cv.waitKey(10) == 27: # escape
-                        stop = True
-                        break
-                
-                # new_feature_fr.keypoints = itemgetter(*[x.queryIdx for x in good])(new_feature_fr.keypoints)
-                # new_feature_fr.descriptors = itemgetter(*[x.queryIdx for x in good])(new_feature_fr.descriptors)
-                feature_collection.append(new_feature_fr)
-                feature_votes.append(votes)
-                # feature_matches.append(np.stack([np.array([m.queryIdx, m.trainIdx]) for m in good], axis=0))
-            else:
-                feature_collection.append(new_feature_fr)
+            fextractor.add_feature_frame(frame)
 
         frame_id += 1
 
-    # print(feature_matches)
+    fextractor.reduce_features()
 
-    # for i in range(len(feature_matches)-1):
-    #     matches01 = feature_matches[i]
-    #     matches12 = feature_matches[i+1]
-    #     feature0 = feature_collection[i]
-    #     feature1 = feature_collection[i+1]
-    #     feature2 = feature_collection[i+2]
-    #     for m in matches01:
-    #         if m.queryIdx in matches12:
-    #             pass
-
-    # print(feature_matches)
-
-    # drop the first args.feature_frames since they were not involved in feature voting
-    feature_collection = feature_collection[args.feature_frames+1:]
-
-    total_features = 0
-    final_features = 0
-    for f in feature_collection:
-        total_features += len(f.descriptors)
-
-    print("Reducing feature count")
-    assert(len(feature_collection) == len(feature_votes))
-    for f, vote in zip(feature_collection, feature_votes):
-        f.descriptors = f.descriptors[vote > 0]
-        f.keypoints = [x for x, y, in zip(f.keypoints, vote) if y > 0]
-
-    for f in feature_collection:
-        final_features += len(f.descriptors)
-
-    print("%i features down from %i" % (final_features, total_features))
-
-    dump_features(feature_collection, args.feature_file)
+    dump_features(fextractor.feature_collection, args.feature_file)
