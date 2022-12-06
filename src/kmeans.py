93c7e02fe1c9d6ed9390e43c389bd8302ee27de3
Dylan Jessen <dylanjessen@system76-pc.localdomain>
2022-12-06 12:26:59 -0800

    cluster v2

--- src/kmeans.py
+++ src/kmeans.py
@@ -5,11 +5,21 @@ import sklearn
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin_min
 
+from typing import Iterable
+from typing import NamedTuple
+from typing import List
+import matplotlib.pyplot as plt
+
+class Frame(NamedTuple):
+    raw: np.ndarray = None
+    kps: np.ndarray = None
+    dcs: np.ndarray = None
+
 def status(msg):
     print(f"\r{msg}: ", end="", flush=True)
 
 # reduce a list of frames to only `k` frames with KMeansClutering
-def cluster_frames(frames, k):
+def cluster_frames_v1(frames, k):
     # compute the descriptors for all the frames
     frames_dcss = []
     for i, f in enumerate(frames):
@@ -39,3 +49,45 @@ def cluster_frames(frames, k):
     print("done")
 
     return new_fs, new_fs_dcss # new frames, and their descriptors
+
+
+
+def __match(query: Frame, train: Frame, threshold):
+    matches = cv2.BFMatcher().knnMatch(query.dcs, train.dcs, k=2)
+
+    return [m for m,n in matches if m.distance < threshold*n.distance]
+
+def most_similar(query: Frame, trains: Iterable[Frame], confidence_floor, threshold) -> Frame:
+    nfeats=len(query.dcs)
+
+    best_score = -1
+    best = None
+    for t in trains:
+        this_score = len(__match(query, t, threshold=threshold))
+        if this_score < nfeats*confidence_floor:
+            # not enough matches to assert the object is even present
+            continue
+        if this_score > best_score:
+            best_score = this_score
+            best = t
+    #print(best_score, f"(required = {nfeats*confidence_floor})")
+    return best
+
+def cluster_frames_v2(frames: List[Frame], k) -> List[Frame]:
+    dcss = list(map(lambda x: x.dcs, frames))
+
+    xs, ys = [], []
+    train = np.array(dcss[0])
+
+    len_dcss = len(dcss)
+    for frameIdx, dcs in enumerate(dcss):
+        this_ys, train = __acc_match(dcs, train)
+        this_xs = [frameIdx]*len(this_ys)
+        
+        ys += this_ys
+        xs += this_xs
+    points = np.array(list(zip(xs, ys)))
+    kmeans = KMeans(n_clusters=k, random_state=1)
+    kmeans.fit(points)
+    best_indexes = pairwise_distances_argmin_min(kmeans.cluster_centers_, points)[0]
+    return list(map(lambda i: frames[i], points[best_indexes, 0]))
